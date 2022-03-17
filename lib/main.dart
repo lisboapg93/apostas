a496f6287b0249e5bf851efd9a55323d87c5d314
Pedro Lisboa <pedrogabriel3@gmail.com>
2022-03-17 01:05:08 -0300

    teste3

--- lib/main.dart
+++ lib/main.dart
@@ -4,10 +4,9 @@ void main() {
   runApp(const MyApp());
 }
 
+
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
-
-  
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -22,17 +21,16 @@ class MyApp extends StatelessWidget {
   }
 }
 
+
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
-
   final String title;
-
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
+
 class _MyHomePageState extends State<MyHomePage> {
-  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
